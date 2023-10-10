4567f4e464201a14aa50b44b49444b775fb067e1
Fukuemon <Fukuemony029@gmail.com>
2023-10-10 09:45:14 +0900

    ✨ モデル共通の作成日時と更新日時のプロパティをまとめたTimeStampedModelの作成 #3

--- apps/accounts/models.py
+++ apps/accounts/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
 
-# Create your models here.
+# 継承することで、作成日と更新日を自動で追加するモデル
+class TimestampedModel(models.Model):
+    created_on = models.DateTimeField(auto_now_add=True)  # 作成日
+    updated_on = models.DateTimeField(auto_now=True)  # 更新日
+
+    class Meta:
+        abstract = True
+        ordering = ['-created_on', '-updated_on']
