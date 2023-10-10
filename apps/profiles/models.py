0d36e3c7136f96ec163377d769d0324e96f909ab
Fukuemon <Fukuemony029@gmail.com>
2023-10-10 20:37:03 +0900

    ✨ プロフィール情報の追加 #3
    account：先ほど作成したUserモデルと紐づける(1:1)
    username：ユーザー名
    img：アバターイメージ
    following：フォローしている人
    followers：フォローされている人
    フォロー関連は多対多の関係で結びつけ、中間テーブルとしてConnectionテーブルを作成

--- apps/profiles/models.py
+++ apps/profiles/models.py
@@ -1,3 +1,49 @@
 from django.db import models
-
 # Create your models here.
+from django.conf import settings
+from apps.accounts.models import TimestampedModel
+
+def upload_avatar_path(instance, filename):
+    ext = filename.split('.')[-1]
+    # 保存先のファイルパスを生成
+    # 生成されたファイルパスは 'avatars/{ユーザープロファイルのID}{ニックネーム}.{拡張子}'という形式になる
+    return '/'.join(['avatars', str(instance.userProfile.id) + str(instance.nickName) + str(".") + str(ext)])
+
+
+class Connection(models.Model):
+    """
+    フォロー/フォロワー機能のための中間テーブル
+    メタ情報： unique_togetherでfollowerとfollowingの組み合わせがユニークになるように設定
+    """
+    follower = models.ForeignKey("Profile", on_delete=models.CASCADE, related_name="following_connection")
+    following = models.ForeignKey('Profile', on_delete=models.CASCADE, related_name='follower_friendships')
+    class Meta:
+        unique_together = ('follower', 'following')
+    def __str__(self):
+        return f"{self.follower} follows {self.following}"
+
+
+class Profile(models.Model, TimestampedModel):
+    """
+    プロフィールモデル： ユーザーのプロフィール情報を管理する
+    """
+    username = models.CharField(max_length=20 ,null=True, blank=True)
+    # Userモデルとリレーション
+    account = models.OneToOneField(
+        settings.AUTH_USER_MODEL, related_name='profile',
+        on_delete=models.CASCADE
+    )
+    img = models.ImageField(blank=True, null=True, upload_to=upload_avatar_path)  # アバター画像
+
+    # フォロー関連のフィールド
+    followings = models.ManyToManyField(
+        'Profile', verbose_name='フォロー中のユーザー', through='Connection',
+        related_name='+', through_fields=('follower', 'following')
+    )
+    followers = models.ManyToManyField(
+        'Profile', verbose_name='フォローされているユーザー', through='Connection',
+        related_name='+', through_fields=('following', 'follower')
+    )
+
+    def __str__(self):
+        return self.nickName
