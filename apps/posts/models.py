ab76544bef2a088b13dcedb019631b192b9c8894
Fukuemon <Fukuemony029@gmail.com>
2023-10-12 22:46:53 +0900

    ✨投稿・タグ・コメントのモデル作成 #4
    (投稿モデル：投稿者、店舗名、メニュー名、評価、値段、メニュー画像、メニュー3Dモデル、レビュー内容、いいね、訪問日)
    (タグモデル：管理側でタグを用意しておく)
    (コメントモデル：コメント、投稿者、)

--- apps/posts/models.py
+++ apps/posts/models.py
@@ -1,3 +1,66 @@
 from django.db import models
+from ..accounts.models import TimestampedModel
+from ..restaurants.models import Restaurant
+from ..profiles.models import Profile
 
-# Create your models here.
+
+
+def upload_post_path(instance, filename):
+    ext = filename.split('.')[-1]
+    return '/'.join(['posts', str(instance.author.id) + str(instance.menu_name) + str(".") + str(ext)])
+
+def upload_model_path(instance, filename):
+    ext = filename.split('.')[-1]
+    return '/'.join(['models', str(instance.author.id) + str(instance.menu_name) + str(".") + str(ext)])
+
+SCORE_CHOICES = [
+    (1, '1'),
+    (2, '2'),
+    (3, '3'),
+    (4, '4'),
+    (5, '5'),
+]
+
+
+class Post(TimestampedModel):
+    """
+    投稿モデル：投稿者、店舗名、メニュー名、評価、値段、メニュー画像、メニュー3Dモデル、レビュー内容、いいね、訪問日
+    """
+    author = models.ForeignKey(
+        Profile, related_name='posts', on_delete=models.CASCADE
+    )  # 投稿者
+    restaurant = models.ForeignKey(
+        Restaurant, related_name='posts', on_delete=models.CASCADE
+    )  # 店舗名
+    tags = models.ManyToManyField('Tag', related_name='posts', blank=True)  # タグ
+    menu_name = models.CharField(max_length=200)  # メニュー名
+    # 評価 1~5
+    score = models.IntegerField("評価", choices=SCORE_CHOICES, default=3)
+    price = models.IntegerField()  # 値段
+    menu_photo = models.ImageField(upload_to=upload_post_path, blank=True, null=True)  # メニュー画像
+    menu_model = models.FileField(upload_to=upload_model_path, blank=True, null=True)  # メニュー3Dモデル
+    review_text = models.TextField(blank=True, null=True)  # レビュー内容
+    likes = models.ManyToManyField(Profile, related_name='likes', blank=True,verbose_name="Likes")  # いいね
+    visited_date = models.DateField(blank=True, null=True)  # 訪問日
+    def __str__(self):
+        return self.menu_name
+
+
+class Tag(TimestampedModel):
+    """
+    タグモデル：タグ
+    """
+    tag = models.CharField(max_length=255)
+    def __str__(self):
+        return self.tag
+
+
+class Comment(TimestampedModel):
+    """
+    コメントモデル：コメント、投稿者、投稿
+    """
+    comment = models.TextField()
+    profile = models.ForeignKey(Profile, on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    def __str__(self):
+        return self.comment
