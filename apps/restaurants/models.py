7c3d9820b506e483d369440a93de53a0e0711a0b
Fukuemon <Fukuemony029@gmail.com>
2023-10-12 22:51:11 +0900

    ✨店舗モデルの作成#4
    店舗名、緯度、経度、都道府県、住所を含む
    HOTPEPPERAPIで取得した店舗情報を格納する

--- apps/restaurants/models.py
+++ apps/restaurants/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+class Restaurant(models.Model):
+    """
+    店舗モデル：店舗名、緯度、経度、都道府県、住所
+    """
+    name = models.CharField(max_length=200)  # 店舗名
+    lat = models.CharField(max_length=200)  # 緯度
+    lng = models.CharField(max_length=200)  # 経度
+    area = models.CharField(max_length=200)  # 都道府県
+    address = models.CharField(max_length=200)  # 住所
+
+    def __str__(self):
+        return self.name
