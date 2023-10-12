db585aa31cb1e44db3ad27f31815f558bb5a33f5
Fukuemon <Fukuemony029@gmail.com>
2023-10-12 13:05:06 +0900

    ✨ urlエンドポイントの追加 #3
    MyAccountViewSetに応じたエンドポイントを追加
    特定のurlしか含めないことで、必要のないメソッドを含めないように

--- apps/accounts/urls.py
+++ apps/accounts/urls.py
@@ -1,7 +1,17 @@
 from rest_framework.routers import DefaultRouter
-from .views import UserViewSet
+from .views import UserViewSet, MyAccountViewSet
+from django.urls import path, include
 
+account_router = DefaultRouter()
+account_router.register('', UserViewSet, basename='user')  # basename：URLの末尾につく名前
 
-router = DefaultRouter()
-router.register('', UserViewSet, basename='user')  # basename：URLの末尾につく名前
-urlpatterns = router.urls
+urlpatterns = [
+    path('me/', MyAccountViewSet.as_view({
+        'get': 'retrieve',
+        'put': 'update',
+        'patch': 'update',
+    }), name='me'),  # ログインユーザー用のアカウント管理エンドポイント
+    path('me/change_password/', MyAccountViewSet.as_view({'post': 'change_password'}),
+         name='myaccount-change-password'),  # パスワード変更のエンドポイント
+    path('', include(account_router.urls)),  # 管理者用のユーザー管理エンドポイント
+]
