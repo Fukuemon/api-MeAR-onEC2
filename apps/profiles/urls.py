07b3f0b6f6e6b97118eb0e2611065836d7e752a5
Fukuemon <Fukuemony029@gmail.com>
2023-10-25 10:52:54 +0900

    ✨:feat #3 ログインユーザーのProfileの更新を行うViewの作成

--- apps/profiles/urls.py
+++ apps/profiles/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path, include
-from apps.profiles.views import ProfileViewSet, MyProfileListView, ProfileFollowView
+from apps.profiles.views import ProfileViewSet, MyProfileView, ProfileFollowView
 from rest_framework.routers import DefaultRouter
 
 app_name = "profile"
@@ -8,7 +8,7 @@ router = DefaultRouter()
 router.register("", ProfileViewSet)
 
 urlpatterns=[
-    path("me/", MyProfileListView.as_view(), name="myprofile"),
+    path("me/", MyProfileView.as_view(), name="myprofile"),
     path('follow/<int:account_id>/', ProfileFollowView.as_view(), name='follow-user'),
     path("",include(router.urls)),
 ]
