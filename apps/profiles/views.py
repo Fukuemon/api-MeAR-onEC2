a58006c90a628a58ce330adad29852f8d7d8ab43
Fukuemon <Fukuemony029@gmail.com>
2023-11-06 18:56:05 +0900

    🩹typo: import文追加

--- apps/profiles/views.py
+++ apps/profiles/views.py
@@ -1,11 +1,11 @@
 from rest_framework.response import Response
-from rest_framework.permissions import IsAuthenticated
 from rest_framework import status
 from django.shortcuts import get_object_or_404
 from rest_framework import generics
 from rest_framework import viewsets
 from rest_framework.views import APIView
 from django.http import Http404
+from rest_framework.permissions import AllowAny, IsAuthenticated
 
 from .models import Profile, Connection
 from .serializers import ProfileSerializer, ProfilePatchSerializer
